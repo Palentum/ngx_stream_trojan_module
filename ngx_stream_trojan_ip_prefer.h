5d890137908783be184b77d87c240c0d27c31092
Palentum <79781425+Palentum@users.noreply.github.com>
2026-06-11 04:55:53 +0800

    Remove unused IP prefer helper

--- ngx_stream_trojan_ip_prefer.h
+++ ngx_stream_trojan_ip_prefer.h
@@ -1,39 +1,8 @@
 #ifndef NGX_STREAM_TROJAN_IP_PREFER_H
 #define NGX_STREAM_TROJAN_IP_PREFER_H
 
-#include <stddef.h>
-
 #define NGX_STREAM_TROJAN_IP_PREFER_AUTO 0
 #define NGX_STREAM_TROJAN_IP_PREFER_IPV4 1
 #define NGX_STREAM_TROJAN_IP_PREFER_IPV6 2
-
-static inline size_t
-ngx_stream_trojan_ip_prefer_select(const int *families, size_t n,
-    int prefer, int ipv4_family, int ipv6_family)
-{
-    size_t i;
-
-    if (families == NULL || n == 0) {
-        return n;
-    }
-
-    if (prefer == NGX_STREAM_TROJAN_IP_PREFER_IPV4) {
-        for (i = 0; i < n; i++) {
-            if (families[i] == ipv4_family) {
-                return i;
-            }
-        }
-    }
-
-    if (prefer == NGX_STREAM_TROJAN_IP_PREFER_IPV6) {
-        for (i = 0; i < n; i++) {
-            if (families[i] == ipv6_family) {
-                return i;
-            }
-        }
-    }
-
-    return 0;
-}
 
 #endif
